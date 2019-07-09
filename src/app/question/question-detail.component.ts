82563ecdff746b98b5b283fa7a53e23ba61cbe86
PedroMacias <pedro.maciasl@outlook.com>
2019-07-08 21:43:34 -0500

    question module

--- src/app/question/question-detail.component.ts
+++ src/app/question/question-detail.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { Question } from './question.model';
 
 @Component({
   selector: 'app-question-detail',
-  templateUrl: './question-detail.component.html'
+  templateUrl: './question-detail.component.html',
+  styleUrls: ['./question-detail.component.css']
 })
 export class QuestionDetailComponent {
 
+question: Question = new Question(
+  'Esta es una nueva pregunta',
+  'tengo muchas preguntas',
+  new Date(),
+  'devicon-android-plain'
+);
 }
