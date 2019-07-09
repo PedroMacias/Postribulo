8db1bfd3d162234e11c8a8e17c76141fcdf65698
PedroMacias <pedro.maciasl@outlook.com>
2019-07-08 20:42:18 -0500

    First Commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import {
+  MatToolbarModule,
+  MatIconModule
+} from '@angular/material';
+
+const modules = [
+  MatToolbarModule,
+  MatIconModule
+];
+
+@NgModule({
+  imports: modules,
+  exports: modules
+})
+export class MaterialModule {}
