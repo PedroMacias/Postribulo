2a37a56ac6f69bfcf720f258e7765c9924258a1a
PedroMacias <pedro.maciasl@outlook.com>
2019-09-11 21:33:33 -0500

    add new modules to server

---
+++ src/app/question/question.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Question } from './question.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import urljoin from 'url-join';
+import 'rcjs/add/operator/toPromise';
+
+@Injectable()
+export class QuestionService {
+
+private questionsUrl: string;
+
+constructor(private http: HttpClient) {
+  this.questionsUrl = urljoin(environment.apiUrl, 'questions');
+}
+
+getQuestions(): Promise<void | Question[]> {
+  return this.http.get(this.questionsUrl)
+            .toPromise()
+            .then(response => JSON.parse(JSON.stringify(response as Question[])))
+            .catch(this.handleError);
+}
+
+handleError() {
+}
+}
